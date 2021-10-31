1f564af887e800ac50c4291a2e6a74da209a905c
Konrad Pagacz <konrad.pagacz@gmail.com>
2021-10-31 17:41:50 +0100

    added the option to allow hosting dev server on the ec2 instance

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,5 +1,8 @@
 module.exports = {
     devServer: {
-        port: 3000
+        port: 3000,
+        allowedHosts: [
+            'ec2-3-70-202-40.eu-central-1.compute.amazonaws.com'
+        ]
     }
 }
