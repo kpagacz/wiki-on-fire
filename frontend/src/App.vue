1524907671b4e415032d1301d71b59a55e5a416c
karol.kozlo <karol.kozlowski1999@gmail.com>
2021-11-01 23:31:37 +0100

    [22_common_styles]
    [add] Install less, less-loader, css-loader
    [add] Create common styles less file
    [add] Roboto font in index.html and used in App

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <main>
     <router-view msg="This is a basic frontend"></router-view>
+    df
   </main>
 </template>
 
@@ -10,3 +11,15 @@ export default {
   name: "App",
 };
 </script>
+
+<style lang="less">
+body {
+  margin: 0;
+  box-sizing: border-box;
+
+  main {
+    font-family: Roboto;
+  }
+}
+
+</style>
