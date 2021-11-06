50ff612f3fa1265240431b4681345f13e3051b8a
karol.kozlo <karol.kozlowski1999@gmail.com>
2021-11-06 19:03:24 +0100

    [20_Footer]
    [add] New WofFooter component placed in App

---
+++ frontend/src/components/WofFooter.vue
@@ -0,0 +1,45 @@
+<template>
+    <footer>
+        <img class="footer__ul-img" src="../assets/logo-ul-eng.png">
+        <div class="footer__authors">
+            <h4>Authors:</h4>
+            <span>Karol Kozłowski</span>
+            <span>Konrad Pagacz</span>
+            <span>Robert Szczurkowski</span>
+            <span>Magdalena Wiechecka</span>
+            <span>Kacper Fryczak</span>
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'WofFooter',
+}
+</script>
+
+<style lang="less">
+@import './common.less';
+
+footer {
+    display: flex;
+    background-color: @primary-theme-color;
+    padding: 10px;
+
+    .footer__ul-img {
+        width: 14em;
+    }
+
+    .footer__authors {
+        display: flex;
+        flex-direction: column;
+        color: @white;
+
+        h4 {
+            margin: 0px;
+            margin-bottom: 4px;
+        }
+    }
+}
+
+</style>
