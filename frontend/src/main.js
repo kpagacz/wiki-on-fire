54d169c046f8d0cda718200d226bac87bd00b656
karol.kozlo <karol.kozlowski1999@gmail.com>
2021-11-09 19:08:30 +0100

    [68_Icons]
    [add] New WofIcon component added globally
    [add] New icons.less style file with embeded font

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import WofCard from './components/WofCard.vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
+import WofIcon from './components/WofIcon.vue';
 import WofHelloWorld from './components/WofHelloWorld.vue';
 const router = createRouter({
     history: createWebHistory(),
@@ -14,4 +15,5 @@ const router = createRouter({
 const app = createApp(App);
 app.use(router);
 app.component('WofCard', WofCard);
+app.component('WofIcon', WofIcon);
 app.mount('#app')
