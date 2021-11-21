d2ae1fd0b9b424b91e94e6e0b8cf8a2e65a18633
Robert <robert10290@gmail.com>
2021-11-21 21:49:20 +0000

    added WofPopper component

---
+++ frontend/src/components/WofPopper.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="popper"/>
+</template>
+
+<script>
+export default{
+    name: "WofPopper"
+}
+</script>
+
+<style lang="less">
+@import './common.less';
+
+.popper{
+    position: fixed;
+    z-index: 9998;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: table;
+}
+
+</style>
