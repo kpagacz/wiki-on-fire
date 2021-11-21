e9b3958cbca4be5351c52de86d4888a628569eb9
Robert <robert10290@gmail.com>
2021-11-21 21:49:44 +0000

    modified App.vue for testing

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,8 +2,16 @@
   <main>
     <wof-header/>
     <div class="main-wrapper">
-      <router-view msg="This is a basic frontend"></router-view>
+      <router-view msg="This is a basic frontend">
+        <wof-button @click="showBox('info')">Open info box</wof-button>
+        <wof-button @click="showBox('warning')">Open warning box</wof-button>
+        <wof-button @click="showBox('success')">Open success box</wof-button>
+        <wof-button @click="showBox('error')">Open error box</wof-button>
+        
+      </router-view>
+      
     </div>
+    <wof-info-box title="Tytuł" v-if="this.isBoxVisible" :type="this.type" @close="closeBox" />
     <wof-footer/>
   </main>
 </template>
@@ -11,10 +19,27 @@
 <script>
 import WofFooter from './components/WofFooter.vue';
 import WofHeader from './components/WofHeader.vue';
+import WofInfoBox from './components/WofInfoBox.vue';
 
 export default {
-  components: { WofFooter, WofHeader },
+  components: { WofFooter, WofHeader, WofInfoBox },
   name: "App",
+  data(){
+    return{
+      isBoxVisible: false,
+      type: String
+    }
+  },
+  methods:{
+    showBox(i){
+      this.isBoxVisible=true;
+      this.type=i;
+    },
+    closeBox(i){
+      this.isBoxVisible=false;
+      this.type=i;
+    }
+  }
 };
 </script>
 
