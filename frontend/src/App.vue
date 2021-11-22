f025e152f89a6c64f6b7a0810146665b0061deee
kpagacz <konrad.pagacz@gmail.com>
2021-11-23 00:12:18 +0100

    Revert "add(WofSpinner): added the WofSpinner component"
    This reverts commit 727a4e16520ff42a48dd2059d5ab2e97ba1499bc.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,7 +4,6 @@
     <div class="main-wrapper">
       <router-view msg="This is a basic frontend"></router-view>
     </div>
-    <wof-spinner color = "red" loading = true size =16 />
     <wof-footer/>
   </main>
 </template>
@@ -12,10 +11,9 @@
 <script>
 import WofFooter from './components/WofFooter.vue';
 import WofHeader from './components/WofHeader.vue';
-import WofSpinner from "./components/WofSpinner.vue";
 
 export default {
-  components: { WofFooter, WofHeader, WofSpinner},
+  components: { WofFooter, WofHeader },
   name: "App",
 };
 </script>
