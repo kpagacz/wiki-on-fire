c9c0cb77f9dcccfccdb127c5fc6683d0969615c2
kpagacz <konrad.pagacz@gmail.com>
2021-11-24 22:37:04 +0100

    renamed wofApi.js

--- frontend/src/httpLayers/wofApi.js
+++ frontend/src/httpLayers/wofApi.js
@@ -1,8 +1,14 @@
 import axios from "axios";
 
 const wofApi = axios.create({
+<<<<<<< Updated upstream
   baseURL: "http://localhost:3001/api/",
   timeout: 5000
+=======
+  baseURL: "http://localhost:3001/api",
+  timeout: 5000,
+  withCredentials: true,
+>>>>>>> Stashed changes
 })
 
 export default wofApi;
