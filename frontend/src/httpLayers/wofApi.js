ff14f6d36ec2561436fa1af98fa142d9ef4d07e2
kpagacz <konrad.pagacz@gmail.com>
2021-11-26 22:17:45 +0100

    wofApi

--- frontend/src/httpLayers/wofApi.js
+++ frontend/src/httpLayers/wofApi.js
@@ -1,11 +1,7 @@
 import axios from "axios";
 
 const wofApi = axios.create({
-<<<<<<< HEAD:frontend/src/httpLayers/wofApi.js
   baseURL: "http://localhost:3001/api",
-=======
-  baseURL: "http://localhost/api/",
->>>>>>> 103_create_login_endpoint:frontend/src/httpLayers/wofapi.js
   timeout: 5000,
   withCredentials: true,
 })
