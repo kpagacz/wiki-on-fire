77d122f7eeaa8b828e5117dcf514a3ef7c73890f
kpagacz <konrad.pagacz@gmail.com>
2021-11-26 20:41:56 +0100

    merged login endpoint

---
+++ frontend/src/httpLayers/login.http.js
@@ -0,0 +1,21 @@
+import wofApi from "./wofApi.js";
+
+const loginUser = async (username, password) => {
+  const token = await wofApi
+    .post("/login", { username: username, password: password })
+    .then(() => {
+      return true;
+    })
+    .catch((error) => {
+      if (error.response) {
+        throw new Error(`${error.response.data.message}`);
+      } else if (error.request) {
+        throw new Error("Service refused connection");
+      } else {
+        throw new Error("Undefined error");
+      }
+    });
+  console.dir(token);
+}
+
+export { loginUser };
