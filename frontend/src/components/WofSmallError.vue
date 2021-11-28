16a0dce882b7e8d5a5c3fd165d7c56d1fb75ce40
Robert <robert10290@gmail.com>
2021-11-28 04:35:14 +0000

    [added] component WofSmallError prototype

---
+++ frontend/src/components/WofSmallError.vue
@@ -0,0 +1,55 @@
+<template>
+<div class="small-error-contener">
+    <div class="small-error-icon">
+        <wof-icon @mouseover="hover = true" @mouseleave="hover = false" icon="wof-warning-circle-fill" :size="this.size"/>
+    </div>
+    <div class="small-error-content" v-if="hover">
+        <slot></slot>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "WofSmallError",
+
+  props: {
+      size: Number
+  },
+  data() {
+    return {
+      hover: false
+    };
+  }
+}
+</script>
+
+<style lang="less">
+
+.small-error-contener{
+    display: flex;
+    position: absolute;
+    align-items: center;
+}
+
+.small-error-icon{
+    color: #E13E3E;
+    margin-right: 0.5em;
+    order: 1;
+    
+}
+
+.small-error-content{
+    border-radius: 1em;
+    padding: 0.5em;
+    font-size: 1rem;
+    background-color: #E13E3E;
+    color: #FFFFFF;
+    order: 2;
+    flex-basis: auto;
+    min-height: 0%;
+    min-width: 0%;
+    align-content: center;
+}
+
+</style>
