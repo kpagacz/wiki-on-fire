2d3f0ada205b8c53ecd92f6075742e4e6858e127
karol.kozlo <karol.kozlowski1999@gmail.com>
2021-12-02 12:27:49 +0100

    [29_Optional_Section]
    [add] New WofOptionalSection component with basic styling
    [add] Custom toggle button in WofOptionalSection
    [remove] Padding from main wrapper in App

---
+++ frontend/src/components/WofOptionalSection.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="optional">
+    <div class="optional__content"></div>
+    <div class="optional__button-bar">
+      <div class="optional__toggle-button">
+        <wof-icon icon="wof-down" :size="2"></wof-icon>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "WofOptionalSection",
+};
+</script>
+
+<style lang="less">
+@import "./common.less";
+
+.optional {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+
+  .optional__content {
+    width: 100%;
+    height: 300px;
+    background-color: @secondary-color;
+  }
+
+  .optional__button-bar {
+      display: flex;
+      justify-content: center;
+      background-color: @secondary-color;
+      width: 100%;
+      height: 22px;
+
+    .optional__toggle-button {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      border-radius: 50%;
+      width: 30px;
+      height: 30px;
+      padding: 7px;
+      background-color: @primary-accent-color;
+      color: @primary-bright-text-color;
+    }
+  }
+}
+</style>
