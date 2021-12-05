c715ed3b8c2e16e0f8b0d5ac1a5c6b1cba565986
karol.kozlo <karol.kozlowski1999@gmail.com>
2021-12-05 11:24:23 +0100

    [29_Optional_Section]
    [add] Slot inside WofOptionalSection

--- frontend/src/components/WofOptionalSection.vue
+++ frontend/src/components/WofOptionalSection.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="optional">
-    <div class="optional__content"></div>
+      <div class="optional__content" v-if="isContentVisible">
+        <slot></slot>
+      </div>
     <div class="optional__button-bar">
       <div class="optional__toggle-button">
-        <wof-icon icon="wof-down" :size="2"></wof-icon>
+        <wof-icon :icon="toggleIcon" :size="2" @click="toggleContent"></wof-icon>
       </div>
     </div>
   </div>
@@ -12,6 +14,24 @@
 <script>
 export default {
   name: "WofOptionalSection",
+  data() {
+    return {
+      isContentVisible: true
+    };
+  },
+  methods: {
+    toggleContent() {
+      this.isContentVisible = !this.isContentVisible;
+    }
+  },
+  computed: {
+    toggleIcon() {
+      if(this.isContentVisible) {
+        return 'wof-up';
+      }
+      return 'wof-down';
+    }
+  }
 };
 </script>
 
@@ -25,6 +45,8 @@ export default {
   width: 100%;
 
   .optional__content {
+    display: flex;
+    justify-content: center;
     width: 100%;
     height: 300px;
     background-color: @secondary-color;
