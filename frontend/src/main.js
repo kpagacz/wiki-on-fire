875bf722c5a79eed7cb13f2eb08f94f3594d469a
Robert <robert10290@gmail.com>
2021-12-15 10:34:09 +0000

    main.js rollback

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,9 +7,6 @@ import WofCard from './components/WofCard.vue';
 import WofIcon from './components/WofIcon.vue';
 import WofButton from './components/WofButton.vue';
 
-import WofIcon from './components/WofIcon.vue';
-import WofButton from './components/WofButton.vue';
-
 const app = createApp(App);
 app.use(router);
 app.use(store);
