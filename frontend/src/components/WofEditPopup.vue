1931f500ec101c80424aab5ec0898a0f37158cfa
Robert <robert10290@gmail.com>
2021-12-15 15:37:51 +0000

    [change] margin-top value

--- frontend/src/components/WofEditPopup.vue
+++ frontend/src/components/WofEditPopup.vue
@@ -129,7 +129,7 @@ export default {
             display: flex;
             flex-direction: row;
             justify-content: space-between;
-            margin-top: 5px;
+            margin-top: 10px;
         }
     }
     .loading{
