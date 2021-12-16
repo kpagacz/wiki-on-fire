c7a328b2227c78c87a036ea8a26193607646169a
karol.kozlo <karol.kozlowski1999@gmail.com>
2021-12-16 18:02:27 +0100

    [35_User_Options]
    [add] New UserPage component
    [add] UserPage component set as component for route /user/:userLogin

---
+++ frontend/src/pages/UserPage.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="login-page">
+        <wof-optional-section>
+            <div class="login-page__user">
+                Username: {{ userLogin }}
+            </div>
+        </wof-optional-section>
+    </div>
+</template>
+
+<script>
+import WofOptionalSection from '../components/WofOptionalSection.vue';
+
+export default {
+    name: 'UserPage',
+    components: {
+        WofOptionalSection,
+    },
+    props: {
+        userLogin: {},
+        
+    }
+};
+</script>
+
+<style lang="less">
+@import '../components/common.less';
+
+.login-page {
+    display: flex;
+    width: 100%;
+}
+
+</style>
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -4,13 +4,14 @@ import store from './store.js';
 import WofHelloWorld from './components/WofHelloWorld.vue';
 import RegisterPage from './pages/RegisterPage.vue';
 import LoginPage from './pages/LoginPage.vue';
+import UserPage from './pages/UserPage.vue';
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/', component: WofHelloWorld },
         { path: '/login', component: LoginPage, meta: {requiresUnAuth: true}},
-        { path: '/user/:userLogin', redirect: '/', props: true, meta: {requiresAuth: true}},
+        { path: '/user/:userLogin', component: UserPage, props: true, meta: {requiresAuth: true}},
         { path: '/register', component: RegisterPage, meta: {requiresUnAuth: true} },
         { path: '/:notFound(.*)', redirect: '/' }
     ]
