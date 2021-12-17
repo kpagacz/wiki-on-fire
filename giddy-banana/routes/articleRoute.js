f903203919cd009afeb796814ef473ccc49a4222
mwiech99 <80545933+mwiech99@users.noreply.github.com>
2021-12-17 16:50:08 +0100

    article - controller, service, route

---
+++ giddy-banana/routes/articleRoute.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { getArticles, postArticles, deleteArticles, updateArticles } from "../controllers/articleController.js";
+
+const articlesRouter = new express.Router();
+articlesRouter.use(express.json());
+articlesRouter.route("/articles/:title")
+  .get(getArticles)
+  .delete(deleteArticles)
+  .patch(updateArticles);
+articlesRouter.route("/articles").post(postArticles);
+
+export default articlesRouter;
