7b002db19db21f467015daf17cf63a1a68fba79e
mwiech99 <80545933+mwiech99@users.noreply.github.com>
2021-12-17 20:56:19 +0100

    article - controller, service, route - v2

--- giddy-banana/routes/index.js
+++ giddy-banana/routes/index.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import usersRouter from "./usersRoute.js";
 import loginRouter from "./loginRoute.js";
+import articlesRouter from "./articleRoute.js";
 
 // define /api/* routers
 const mainRouter = new Router();
 mainRouter.use(usersRouter);
 mainRouter.use(loginRouter);
+mainRouter.use(articlesRouter);
 
 // define /api router
 const apiRouter = new Router();
