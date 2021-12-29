3ee16a03ad5c36ecd2a58fac44ad85c01546f9e4
karol.kozlo <karol.kozlowski1999@gmail.com>
2021-12-29 16:21:47 +0100

    [27_Article_List]
    [remove] Test code in App

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,9 +2,7 @@
   <main>
     <wof-header/>
     <div class="main-wrapper">
-      <router-view msg="This is a basic frontend">
-          <wof-article-list :articles="articles" :isButtonVisible="true" @add="addArticle"></wof-article-list>
-      </router-view>
+      <router-view msg="This is a basic frontend"></router-view>
     </div>
     <wof-footer/>
   </main>
@@ -13,49 +11,10 @@
 <script>
 import WofFooter from './components/WofFooter.vue';
 import WofHeader from './components/WofHeader.vue';
-import WofArticleList from './components/WofArticleList.vue';
 
 export default {
-  components: { WofFooter, WofHeader, WofArticleList },
+  components: { WofFooter, WofHeader },
   name: "App",
-  data() {
-    return {
-      articles: [
-        {
-          id: 1,
-          title: 'Some interesting article1',
-          linkToContents: 'https://en.wikipedia.org/wiki/Potato',
-          dateAdded: '24.12.2021',
-          lastModification: '27.12.2021',
-          numberOfViews: 3401,
-          numberOfEditions: 7
-        },
-        {
-          id: 2,
-          title: 'Best City at Mediterranean Sea Ever!!!!1!',
-          linkToContents: 'https://en.wikipedia.org/wiki/Constantinople',
-          dateAdded: '25.12.2021',
-          lastModification: '28.12.2021',
-          numberOfViews: 6543,
-          numberOfEditions: 10
-        },
-        {
-          id: 3,
-          title: 'I know it from college',
-          linkToContents: 'https://en.wikipedia.org/wiki/Turing_machine',
-          dateAdded: '27.12.2021',
-          lastModification: '22.12.2021',
-          numberOfViews: 245,
-          numberOfEditions: 1
-        },
-      ]
-    }
-  },
-  methods: {
-    addArticle(articleID) {
-      console.log(articleID);
-    }
-  }
 };
 </script>
 
