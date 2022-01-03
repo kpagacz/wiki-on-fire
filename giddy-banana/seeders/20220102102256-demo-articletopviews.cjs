f0d610497b2832ac7408711ccf14c7df9a925591
mwiech99 <80545933+mwiech99@users.noreply.github.com>
2022-01-03 14:54:02 +0100

    seeds for loop2

--- giddy-banana/seeders/20220102102256-demo-articletopviews.cjs
+++ giddy-banana/seeders/20220102102256-demo-articletopviews.cjs
@@ -14,12 +14,10 @@ module.exports = {
 
     var arttopviews = [];
     var s2 = '.12.2021';
-    //var days = ['17','18','19','20','21','22','23','24','25','26','27','28','29','30','31'];
 
     function generateRandomInteger(min, max) {
       return Math.floor(min + Math.random()*(max + 1 - min))
     }
-
 
 
     for (let i = 0; i < 100; i++) {
@@ -27,7 +25,6 @@ module.exports = {
       var s1 = num.toString();
       arttopviews.push({
                 ArticleId: i + 1,
-                //date: days[Math.round(Math.random())].concat(s2),
                 date: s1.concat(s2),
                 rank_position: 11,
                 number_of_views: 1120,
@@ -37,22 +34,6 @@ module.exports = {
     }
 
     await queryInterface.bulkInsert("ArticleTopViews", arttopviews);
-
-
-
-
-
-
-//    await queryInterface.bulkInsert("ArticleTopViews", [
-//      {
-//        ArticleId: 1,
-//        date: new Date(),
-//        rank_position: 16,
-//        number_of_views: 1760,
-//        createdAt: new Date(),
-//        updatedAt: new Date()
-//      }
-//    ])
   },
 
   down: async (queryInterface, Sequelize) => {
