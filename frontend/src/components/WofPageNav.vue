5756a660dd1b22099f88a8b0359f1e5eb57cbe20
karol.kozlo <karol.kozlowski1999@gmail.com>
2022-01-04 20:08:03 +0100

    [174_Page_Nav]
    [add] New prevButtonState computed property in WofPageNav
    [add] New nextButtonState computed property in WofPageNav
    [add] Styling for WofPageNav
    [add] New emits: 'prev', 'next' in WofPageNav

--- frontend/src/components/WofPageNav.vue
+++ frontend/src/components/WofPageNav.vue
@@ -1,10 +1,20 @@
 <template>
     <div class="wof-page-nav">
-        <wof-button icon="wof-left" :size="1.5"></wof-button>
+        <wof-button icon="wof-left"
+                    :size="1.4"
+                    :variant="prevButtonState"
+                    @click="this.$emit('prev', this.currentPage-1)">
+        </wof-button>
         <div class="wof-page-nav__pages">
             <input class="wof-page-nav__input" type="number" :min="1" :max="totalPages" v-model="value">
+            <span>of</span>
+            <span class="wof-page-nav__total-pages">{{ totalPages }}</span>
         </div>
-        <wof-button icon="wof-right" :size="1.5"></wof-button>
+        <wof-button icon="wof-right"
+                    :size="1.4"
+                    :variant="nextButtonState"
+                    @click="this.$emit('next', this.currentPage+1)">
+        </wof-button>
     </div>
 </template>
 
@@ -21,10 +31,25 @@ export default {
             required: true
         }
     },
+    emits: ['prev', 'next'],
     data() {
         return {
             value: this.currentPage
         };
+    },
+    computed: {
+        prevButtonState() {
+            if(this.currentPage-1 < 1) {
+                return 'disabled';
+            }
+            return 'default';
+        },
+        nextButtonState() {
+             if(this.currentPage+1 > this.totalPages) {
+                return 'disabled';
+            }
+            return 'default';
+        }
     }
 };
 </script>
@@ -34,21 +59,47 @@ export default {
 
 .wof-page-nav {
     display: flex;
-    padding: 0.5em;
+    justify-content: center;
+    align-items: center;
+    padding: 0.5em 1em;
     background-color: @primary-theme-color;
     border-radius: 20px;
     width: min-content;
     height: min-content;
 
     .wof-page-nav__pages {
+        display: flex;
+        color: @primary-bright-text-color;
+        padding: 0em 1em;
 
         .wof-page-nav__input {
+            font-size: 1rem;
+            font-weight: 600;
+            text-align: center;
             border: none;
+            border-radius: 15px;
             box-shadow: inset 0px 1px 8px rgba(0, 0, 0, 0.2);
+            -moz-appearance: textfield;
+
+            &::-webkit-outer-spin-button,
+            &::-webkit-inner-spin-button {
+                -webkit-appearance: none;
+                margin: 0;
+            }
 
             &:focus {
                 box-shadow: inset 0px 1px 8px rgba(0, 0, 0, 0.2);
+                outline: none;
             }
+        }
+
+        .wof-page-nav__total-pages {
+            font-weight: 700;
+            margin: 0px 0px;
+        }
+
+        span {
+            margin: 0px 5px;
         }
     }
 }
