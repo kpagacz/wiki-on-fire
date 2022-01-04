e31a8eb5119209c2ce34d0e767cb1a4f2b06ee30
karol.kozlo <karol.kozlowski1999@gmail.com>
2022-01-04 18:13:13 +0100

    [174_Page_Nav]
    [add] New WofPageNav component

---
+++ frontend/src/components/WofPageNav.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="wof-page-nav">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'WofPageLinks',
+    props: {
+        currentPage: {
+            type: Number,
+            default: 1
+        },
+        totalPages: {
+            type: Number,
+            required: true
+        }
+    }
+};
+</script>
+
+<style lang="less">
+@import './common.less';
+
+.wof-page-nav {
+    display: flex;
+    padding: 1em;
+    background-color: @primary-theme-color;
+    border-radius: 20px;
+    width: min-content;
+    height: min-content;
+}
+
+</style>
