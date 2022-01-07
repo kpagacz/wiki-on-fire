0894d5b97d2a767ca62eb3d8ec9506d24c0c9015
Konrad Pagacz <konrad.pagacz@gmail.com>
2022-01-07 23:52:42 +0100

    Update wofApi.js

--- frontend/src/httpLayers/wofApi.js
+++ frontend/src/httpLayers/wofApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const wofApi = axios.create({
-  baseURL: "http://localhost/api",
+  baseURL: "http://ec2-18-159-108-123.eu-central-1.compute.amazonaws.com/api",
   timeout: 5000,
   withCredentials: true,
 })
