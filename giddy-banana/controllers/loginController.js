1116851216c7aede65e2fc59fdd117b25ecb16c6
Konrad Pagacz <konrad.pagacz@gmail.com>
2022-01-08 00:10:16 +0100

    Update loginController.js

--- giddy-banana/controllers/loginController.js
+++ giddy-banana/controllers/loginController.js
@@ -20,34 +20,36 @@ import {
  * @name loginController
  */
 export default async (req, res) => {
-  if (req.body.username === undefined || req.body.password === undefined)
+  if (req.body.username === undefined || req.body.password === undefined) {
     res
       .status(400)
       .send({ message: "username and password must be included in the body" });
-  try {
-    const userDataAndToken = await loginUser(
-      req.body.username,
-      req.body.password
-    );
-    const token = (({ token_type, token }) => ({ token_type, token }))(
-      userDataAndToken
-    );
-    res.cookie("wofToken", token, {
-      httpOnly: true,
-      sameSite: "lax",
-      maxAge: 8 * 60 * 60 * 100,
-      domain: "localhost",
-    });
-    const user = userDataAndToken.user;
-    delete user.dataValues.password;
-    res.status(200).send(user);
-  } catch (error) {
-    if (error instanceof InvalidArgumentException)
-      res.status(400).send({ message: "Invalid username or password type" });
-    if (error instanceof InvalidPasswordException)
-      res.status(400).send({ message: "Invalid password" });
-    if (error instanceof NotFoundException)
-      res.status(400).send({ message: "User not found" });
-    res.status(400).send({ message: "Error logging in" });
+  } else {
+    try {
+      const userDataAndToken = await loginUser(
+        req.body.username,
+        req.body.password
+      );
+      const token = (({ token_type, token }) => ({ token_type, token }))(
+        userDataAndToken
+      );
+      res.cookie("wofToken", token, {
+        httpOnly: true,
+        sameSite: "lax",
+        maxAge: 8 * 60 * 60 * 100,
+        domain: "localhost",
+      });
+      const user = userDataAndToken.user;
+      delete user.dataValues.password;
+      res.status(200).send(user);
+    } catch (error) {
+      if (error instanceof InvalidArgumentException)
+        res.status(400).send({ message: "Invalid username or password type" });
+      if (error instanceof InvalidPasswordException)
+        res.status(400).send({ message: "Invalid password" });
+      if (error instanceof NotFoundException)
+        res.status(400).send({ message: "User not found" });
+      res.status(400).send({ message: "Error logging in" });
+    }
   }
 };
