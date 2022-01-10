03c08359b7e6974e3ee346a4dc3ddf65f081bbd8
kpagacz <konrad.pagacz@gmail.com>
2022-01-10 03:55:53 +0100

    fixed the lambda processing the article views

--- amazon-architecture/lambdas/process-dynamo-views/index.js
+++ amazon-architecture/lambdas/process-dynamo-views/index.js
@@ -1,33 +1,56 @@
+// Important considerations for this file and things I have learned
+// when using MySQL library, node and asynchronous calls
+// 1. Wrap calls to connection.query with a Promise and await for its resolution
+// 2. forEach is inadequate to run async functions, use Promise.all(array.map(() => {})) instead
 import { createConnection } from "mysql";
 
-const insertArticle = (article, conn) => {
-  const sql =
-    "INSERT INTO Articles (title, link_to_contents, createdAt, updatedAt) VALUES ?";
-  const values = [
-    [
-      article,
-      "https://en.wikipedia.org/wiki/" + article,
-      new Date()
-        .toISOString()
-        .replace("-", "/")
-        .split("T")[0]
-        .replace("-", "/"),
-      new Date()
-        .toISOString()
-        .replace("-", "/")
-        .split("T")[0]
-        .replace("-", "/"),
-    ],
-  ];
-  return new Promise(function (resolve, reject) {
-    conn.query(sql, [values], function (err, result) {
+const insertArticle = async (article, conn) => {
+  const find_query =
+    "SELECT * FROM Articles WHERE title=" + conn.escape(article);
+  const find_promise = new Promise((resolve, reject) => {
+    conn.query(find_query, function (err, result, fields) {
       if (err) reject(err);
-      else resolve(result);
+      else {
+        resolve(result);
+      }
     });
   });
+
+  const select_result = await find_promise;
+  if (select_result.length > 0) {
+    return select_result[0].id;
+  } else {
+    const sql =
+      "INSERT INTO Articles (title, link_to_contents, createdAt, updatedAt) VALUES ?";
+    const values = [
+      [
+        article,
+        "https://en.wikipedia.org/wiki/" + article,
+        new Date()
+          .toISOString()
+          .replace("-", "/")
+          .split("T")[0]
+          .replace("-", "/"),
+        new Date()
+          .toISOString()
+          .replace("-", "/")
+          .split("T")[0]
+          .replace("-", "/"),
+      ],
+    ];
+    const insert_promise = new Promise((resolve, reject) => {
+      conn.query(sql, [values], function (err, result) {
+        if (err) reject(err);
+        else resolve(result);
+      });
+    });
+
+    const insert_res = await insert_promise;
+    return insert_res.insertId;
+  }
 };
 
-const insertTopViews = (articleId, date, rank, views, conn) => {
+const insertTopViews = async (articleId, date, rank, views, conn) => {
   const sql =
     "INSERT INTO ArticleTopViews (ArticleId, date, rank_position, number_of_views, createdAt, updatedAt) VALUES ?";
   const values = [
@@ -51,44 +74,49 @@ const insertTopViews = (articleId, date, rank, views, conn) => {
 
   conn.query(sql, [values], function (err, result) {
     if (err) err;
+    else {
+      console.log(result);
+    }
   });
 };
 
-const processRecord = (record, conn) => {
+const processRecord = async (record, conn) => {
   const date = record.NewImage.date.S;
   const rank = record.NewImage.rank.N;
   const article = record.NewImage.article.S;
   const views = record.NewImage.views.N;
 
   try {
-    const result = insertArticle(article, conn);
-    insertTopViews(result.insertId, date, rank, views, conn);
+    const id = await insertArticle(article, conn);
+    await insertTopViews(id, date, rank, views, conn);
   } catch (err) {
     console.log(err);
   }
 };
 
-const processRecords = (records, conn) => {
-  records.forEach((record) => {
-    processRecord(record.dynamodb, conn);
-  });
+const processRecords = async (records, conn) => {
+  await Promise.all(
+    records.map(async (record) => {
+      await processRecord(record.dynamodb, conn);
+    })
+  );
 };
 
-export const handler = (event) => {
+export const handler = async (event) => {
   const conn = createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_SCHEMA,
   });
-  conn.connect((err) => {
+  conn.connect(async (err) => {
     if (err) console.log("Error connecting to the database");
     console.log("Database connection established");
-    processRecords(event.Records, conn);
+    await processRecords(event.Records, conn);
     conn.end((err) => {
       if (err) console.log("Error ending the database connection");
-      console.log("Database connection terminated")
-    })
-  })
-  return JSON.stringify({ status: 200, msg:"Success"});
+      console.log("Database connection terminated");
+    });
+  });
+  return JSON.stringify({ status: 200, msg: "Success" });
 };
